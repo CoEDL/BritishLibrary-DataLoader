ff7b5a02d08ec3ade1ba88ef35deed0ab576702f
Marco La Rosa <m@lr.id.au>
2020-08-25 16:18:32 +1000

    revise header - add back button

--- collection-viewer/src/components/Navbar.component.vue
+++ collection-viewer/src/components/Navbar.component.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="bg-cloudburst flex flex-row justify-between p-4">
-        <router-link :to="{ name: 'home' }">
-            <div class="text-white text-2xl">
-                <i class="fas fa-home"></i>
-            </div>
-        </router-link>
-        <router-link :to="{ name: 'home' }">
+        <div
+            v-show="!homePage"
+            class="text-white text-2xl cursor-pointer mr-2"
+            @click="navigate"
+        >
+            <i class="fas fa-arrow-left"></i>
+        </div>
+        <router-link :to="{ name: 'home' }" class="flex-grow">
             <div class="heading-text heading-text-size text-white">
                 True Echoes
             </div>
@@ -13,6 +15,7 @@
         <!-- <router-link :to="{ name: 'search' }">
             <div class="text-white text-2xl"><i class="fas fa-search"></i></div>
         </router-link> -->
+
         <router-link :to="{ name: 'menu' }">
             <div class="text-white text-2xl">
                 <i class="fas fa-bars"></i>
@@ -25,6 +28,16 @@
 export default {
     data() {
         return {};
+    },
+    computed: {
+        homePage: function() {
+            return this.$route.name === "home" ? true : false;
+        },
+    },
+    methods: {
+        navigate() {
+            this.$router.go(-1);
+        },
     },
 };
 </script>
