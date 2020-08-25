de671650874742b23ac9fa77c3b88b287cb053c3
Marco La Rosa <m@lr.id.au>
2020-08-25 13:56:29 +1000

    bugfix hover placement

--- collection-viewer/src/components/Menu.component.vue
+++ collection-viewer/src/components/Menu.component.vue
@@ -1,25 +1,40 @@
 <template>
     <div
         class="flex flex-col heading-text heading-text-size cursor-pointer
-        hover:bg-goldensand"
+        "
     >
-        <router-link :to="{ name: 'home' }" class="flex flex-row p-4">
+        <router-link
+            :to="{ name: 'home' }"
+            class="flex flex-row p-4 hover:bg-goldensand"
+        >
             <div class="flex-grow pt-2">Menu</div>
             <div><i class="fas fa-times fa-2x"></i></div>
         </router-link>
-        <router-link :to="{ name: 'browse' }" class="flex flex-row p-4">
+        <router-link
+            :to="{ name: 'browse' }"
+            class="flex flex-row p-4 hover:bg-goldensand"
+        >
             <div class="flex-grow pt-2">Browse the catalogue</div>
             <div><i class="fas fa-chevron-right fa-2x"></i></div>
         </router-link>
-        <router-link :to="{ name: 'search' }" class="flex flex-row p-4">
+        <router-link
+            :to="{ name: 'search' }"
+            class="flex flex-row p-4 hover:bg-goldensand"
+        >
             <div class="flex-grow pt-2">Search the catalogue</div>
             <div><i class="fas fa-chevron-right fa-2x"></i></div>
         </router-link>
-        <router-link :to="{ name: 'about' }" class="flex flex-row p-4">
+        <router-link
+            :to="{ name: 'about' }"
+            class="flex flex-row p-4 hover:bg-goldensand"
+        >
             <div class="flex-grow pt-2">About True Echoes</div>
             <div><i class="fas fa-chevron-right fa-2x"></i></div>
         </router-link>
-        <router-link :to="{ name: 'partners' }" class="flex flex-row p-4">
+        <router-link
+            :to="{ name: 'partners' }"
+            class="flex flex-row p-4 hover:bg-goldensand"
+        >
             <div class="flex-grow pt-2">Partners and funders</div>
             <div><i class="fas fa-chevron-right fa-2x"></i></div>
         </router-link>
