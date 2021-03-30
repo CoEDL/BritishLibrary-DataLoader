de1c30cd22ffe1f529c8cb6ba35ffd8eeca53330
Marco La Rosa <m@lr.id.au>
2021-03-30 19:45:08 +1100

    bugfix

--- loader/bin/build-windows.sh
+++ loader/bin/build-windows.sh
@@ -7,5 +7,5 @@ docker run --rm -ti \
  -v ${PWD}:/project \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
- -v windows_node_modules:/project/node_modules:delegated
+ -v windows_node_modules:/project/node_modules:delegated \
  electronuserland/builder:wine /bin/bash -c "npm install && ./node_modules/.bin/electron-webpack && ./node_modules/.bin/electron-builder --win --publish always"
