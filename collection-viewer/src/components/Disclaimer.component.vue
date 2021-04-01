1b5721b94b7007f14d68c44b2adec639b6c31c96
Marco La Rosa <m@lr.id.au>
2021-04-01 12:59:30 +1100

    update cultural advice

--- collection-viewer/src/components/Disclaimer.component.vue
+++ collection-viewer/src/components/Disclaimer.component.vue
@@ -1,17 +1,15 @@
 <template>
-    <el-dialog
-        title=""
-        :visible.sync="dialogVisible"
-        width="90%"
-        :before-close="handleClose"
-    >
+    <el-dialog title="" :visible.sync="dialogVisible" width="90%" :before-close="handleClose">
         <div class="flex flex-col">
             <language-selector-component />
-            <div class="text-base md:text-xl break-normal mt-10">
-                {{ content[language][0] }}
-            </div>
-            <div class="text-base md:text-xl my-4 break-normal">
-                {{ content[language][1] }}
+            <div class="mt-10 flex flex-col space-y-4">
+                <div
+                    class="text-base md:text-xl break-normal"
+                    v-for="(msg, idx) of content[language]"
+                    :key="idx"
+                >
+                    {{ msg }}
+                </div>
             </div>
         </div>
     </el-dialog>
@@ -27,16 +25,27 @@ export default {
         return {
             content: {
                 English: [
-                    `Aboriginal and Torres Strait Islander people 
-                    should be aware that this catalogue and website may contain 
-                    images, voices or names of deceased persons in photographs, 
-                    film, audio recordings or printed material.`,
-                    `The collections may also contain recordings of ceremonial, 
-                    sacred or secret materials that may not yet be classified as public.`,
+                    `Torres Strait Islander peoples should be aware that this website 
+                    contains images, voices and names of deceased persons.`,
+                    `This website may include collection materials, or descriptions of 
+                    collection materials, that are secret, sacred or sensitive, or 
+                    have traditional protocols around who can access them. All users 
+                    are warned that this website and linked resources include words and 
+                    descriptions which may be outdated and culturally sensitive. 
+                    Terms that reflect the author’s attitude or that of the period in 
+                    which the item was written may be considered inappropriate or offensive today.`,
                 ],
                 TokPisin: [
-                    `Ol manmeri Aborigines na Torres Straits Islander i mas luksave olsem kalsa laibri na kompyuta rekod i holim ol piksa, vois na nem bilong ol lain i dai pinis na stap insait long ol poto, muvi piksa, toktok rikoding na pepa.`,
-                    `Sampela rikoding insait long ol koleksen i ken holim pasin tumbuna i tambu na stap hait long kamapim long ples klia bilong lukluk o soim long ol manmeri.`,
+                    `Ol manmeri bilong Torres Straits Islands i mas luksave olsem dispela 
+                    websait o kompyuta rekod i holim ol piksa, vois na nem bilong ol lain i dai pinis.`,
+                    `Dispela websait i holim ol kainkain samting insait long koleksen em i 
+                    gat pasin tumbuna i tambu na stap hait long kamapim long ples klia bilong 
+                    lukluk na soim long ol manmeri. Ol bikman na lapun tasol long pasin 
+                    kastom ol i ken tok orait long ol manmeri long lukluk long ol samting i 
+                    tambu o soim samting bilong stap hait long kalsa.`,
+                    `Husat manmeri i yusim websait i mas save olsem ol toktok bilong kalsa 
+                    na ol manmeri i bin raitiim em ol i kamap long taim bipo na sampela 
+                    samting nau bai no stret long lukluk o ken bagarapim tingting long pasin bilong tude.`,
                 ],
             },
         };
